70af421cc9e117d0b1c2495d997a55be5dd20b47
Jumaanah <jumaanahbasheeth@gmail.com>
2026-06-11 15:54:55 +0530

    Razorpay integration

---
+++ app/api/createOrder/route.ts
@@ -0,0 +1,30 @@
+import Razorpay from 'razorpay';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// create the Razorpay instance 
+const razorpay = new Razorpay({
+    key_id: process.env.KEY_ID,
+    key_secret: process.env.KEY_SECRET
+})
+console.log(razorpay);
+
+export async function POST(){
+    try{
+        const order = await razorpay.orders.create({
+        amount: 500 * 100,
+        currency: "INR"
+    });
+
+        // console.log("order:", order);
+        return Response.json({
+            success: true,
+            order
+        })
+    }catch(error:any){
+        return Response.json({
+            error: error.message
+        })
+    }
+    
+}
